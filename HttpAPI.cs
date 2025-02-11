e4a837183c466561579aea2d634585eb9f81fa39
Medhat Elmasry <medhat@elmasry.ca>
2025-02-11 11:06:20 -0800

    added create update delete students

--- HttpAPI.cs
+++ HttpAPI.cs
@@ -43,5 +43,95 @@ namespace Snoopy.Function
             return response;
         }
 
+        [Function("GetStudentById")]
+        public HttpResponseData GetStudentById
+(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "students/{id}")] HttpRequestData req,
+    int id
+)
+        {
+            _logger.LogInformation("C# HTTP GET/posts trigger function processed a request.");
+            var student = _context.Students.FindAsync(id).Result;
+            if (student == null)
+            {
+                var response = req.CreateResponse(HttpStatusCode.NotFound);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteStringAsync("Not Found");
+                return response;
+            }
+            var response2 = req.CreateResponse(HttpStatusCode.OK);
+            response2.Headers.Add("Content-Type", "application/json");
+            response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+            return response2;
+        }
+
+        [Function("CreateStudent")]
+        public HttpResponseData CreateStudent
+        (
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "students")] HttpRequestData req
+        )
+        {
+            _logger.LogInformation("C# HTTP POST/posts trigger function processed a request.");
+            var student = JsonConvert.DeserializeObject<Student>(req.ReadAsStringAsync().Result);
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            var response = req.CreateResponse(HttpStatusCode.Created);
+            response.Headers.Add("Content-Type", "application/json");
+            response.WriteStringAsync(JsonConvert.SerializeObject(student));
+            return response;
+        }
+
+        [Function("UpdateStudent")]
+        public HttpResponseData UpdateStudent
+        (
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "students/{id}")] HttpRequestData req,
+            int id
+        )
+        {
+            _logger.LogInformation("C# HTTP PUT/posts trigger function processed a request.");
+            var student = _context.Students.FindAsync(id).Result;
+            if (student == null)
+            {
+                var response = req.CreateResponse(HttpStatusCode.NotFound);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteStringAsync("Not Found");
+                return response;
+            }
+            var student2 = JsonConvert.DeserializeObject<Student>(req.ReadAsStringAsync().Result);
+            student.FirstName = student2.FirstName;
+            student.LastName = student2.LastName;
+            student.School = student2.School;
+            _context.SaveChanges();
+            var response2 = req.CreateResponse(HttpStatusCode.OK);
+            response2.Headers.Add("Content-Type", "application/json");
+            response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+            return response2;
+        }
+
+        [Function("DeleteStudent")]
+        public HttpResponseData DeleteStudent
+        (
+          [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "students/{id}")] HttpRequestData req,
+          int id
+        )
+        {
+            _logger.LogInformation("C# HTTP DELETE/posts trigger function processed a request.");
+            var student = _context.Students.FindAsync(id).Result;
+            if (student == null)
+            {
+                var response = req.CreateResponse(HttpStatusCode.NotFound);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteStringAsync("Not Found");
+                return response;
+            }
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            var response2 = req.CreateResponse(HttpStatusCode.OK);
+            response2.Headers.Add("Content-Type", "application/json");
+            response2.WriteStringAsync(JsonConvert.SerializeObject(student));
+            return response2;
+        }
+
+
     }
 }
